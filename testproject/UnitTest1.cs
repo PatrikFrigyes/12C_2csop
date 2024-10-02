0e536cc11a5699834783f09a839ca6217ab3c275
PatrikFrigyes <marozsan.patrik0611@gmail.com>
2024-10-02 10:35:45 +0200

    Add project files.

---
+++ testproject/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace testproject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string vartErdem = "Fáter heló";
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp3.Program.Main();
+                var kapottEredm = sw.ToString().Trim();
+
+                Assert.AreEqual(vartErdem, kapottEredm);
+
+                
+            }
+            
+        }
+    }
+}
