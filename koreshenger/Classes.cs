cae73e2853d618ef6b2bf463b20279f731330396
PatrikFrigyes <marozsan.patrik0611@gmail.com>
2024-10-02 11:01:33 +0200

    korhengerelkezdve

---
+++ koreshenger/Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koreshenger
+{
+    class kor
+    {
+        private double sugar, kerület, terület;
+
+        public kor()
+        {
+
+        }
+    }
+}
