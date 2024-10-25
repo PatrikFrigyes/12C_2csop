3a4eab6b67d9f753ea31d4198c6c068b00be01c4
PatrikFrigyes <marozsan.patrik0611@gmail.com>
2024-10-25 13:02:14 +0200

    20224.10.25

--- koreshenger/Program.cs
+++ koreshenger/Program.cs
@@ -10,6 +10,25 @@ namespace koreshenger
     {
         static void Main(string[] args)
         {
+            Kör k1 = new Kör();
+            k1.SetSugar(15);
+            k1.SetTerulet();
+            k1.SetKerulet();
+
+            kiír(k1);
+            
+             
+            Kör k2 = new Kör(58);
+            k2.SetKerulet();
+            k2.SetTerulet();
+
+
+            Console.ReadKey();
+        }
+
+        private static void kiír(Kör k)
+        {
+            Console.WriteLine($"A {k.GetSugar()} sugarú kör kerülete: {k.GetKerulet()}, kerülete: {k.GetTerulet()}");
         }
     }
 }
